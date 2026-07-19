4c49a1a0716ff8b8cb16a063dceaf94c001f11cb
Gloria Ayoola <Ollicks05@gmail.com>
2026-07-19 23:31:54 +0100

    Fix for Vercel deployment crash

--- server.js
+++ server.js
@@ -97,6 +97,10 @@ app.use((req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production' && !process.env.VERCEL) {
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
